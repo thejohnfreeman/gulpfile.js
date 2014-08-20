b1fc7a4c64e4166bfac921bc2ee01ab9c746edb4
John Freeman <jfreeman08@gmail.com>
2014-08-20 08:18:58 -0400

    yargs; sourcemaps; bower; wiredep; clean
    - Use yargs for command line parsing.
    - Install new dependencies after pull (`bower`).
    - Symlink dependencies under build directory (`symlink`).
    - Automatically inject dependencies in index.html (`wiredep`).
    - Copy all source files to build by default.
    - Sourcemaps faithful to source directory structure.
    - Do not jshint dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,146 @@
-var gulp          = require('gulp')
+var gulp           = require('gulp')
 // do not use gulp-load-plugins so that we know our dependencies
-var autoprefixer  = require('gulp-autoprefixer')
-var changed       = require('gulp-changed')
-var connect       = require('connect')
-var csso          = require('gulp-csso')
-var gutil         = require('gulp-util')
-var if_           = require('gulp-if')
-var jshint        = require('gulp-jshint')
-var jshintStylish = require('jshint-stylish')
-var lazypipe      = require('lazypipe')
-var livereload    = require('gulp-livereload')
-var open          = require('open')
-var os            = require('os')
-var rimraf        = require('rimraf')
-var size          = require('gulp-size')
-var template      = require('gulp-template')
-var uglify        = require('gulp-uglify')
-var useref        = require('gulp-useref')
+var autoprefixer   = require('gulp-autoprefixer')
+var bower          = require('bower')
+var changed        = require('gulp-changed')
+var connect        = require('connect')
+connect.livereload = require('connect-livereload')
+var csso           = require('gulp-csso')
+var del            = require('del')
+var extend         = require('extend')
+var gutil          = require('gulp-util')
+var gulpif         = require('gulp-if')
+var jshint         = require('gulp-jshint')
+var jshintStylish  = require('jshint-stylish')
+var livereload     = require('gulp-livereload')
+var ngAnnotate     = require('gulp-ng-annotate')
+var opn            = require('opn')
+var os             = require('os')
+var path           = require('path')
+var rev            = require('gulp-rev')
+var size           = require('gulp-size')
+var sourcemaps     = require('gulp-sourcemaps')
+var symlink        = require('gulp-symlink')
+var template       = require('gulp-template')
+var through        = require('through2')
+var uglify         = require('gulp-uglify')
+var usemin         = require('gulp-usemin')
+var wiredep        = require('wiredep').stream
+var yargs          = require('yargs')
 
-// context for templates; exposes debug === true for some builds
-var config = {
-  // config should be overridden in a config target that executes first
-  src: gutil.env.src || 'app',
-  dst: gutil.env.dst || 'dist',
-  port: gutil.env.port || 0,
+var argv = yargs.argv
+
+var defaultConfig = {
+  src:  'app',
+  dst:  argv.dev ? 'dev' : 'prod',
+  port: 0,
+  dev:  false,
 }
 
-config.html    = config.html    || (config.src + '/*.html')
-config.scripts = config.scripts || (config.src + '/scripts/**/*.js')
-// copy every non-HTML file from top directory
-config.files   = config.files   || [config.src + '/*.*', '!' + config.html]
+// context for templates; exposes dev === true for some builds
+// config should be overridden in a config target that executes first
+var config = extend({}, defaultConfig, argv)
+
+gulp.task('bower', function(cb) {
+  bower.commands
+  .install([])
+  .on('end', function() { cb() })
+})
+
+gulp.task('symlink', ['bower'], function() {
+  return gulp.src('bower_components')
+  .pipe(symlink(config.dst))
+})
+
+gulp.task('wiredep', function() {
+  return gulp.src('index.html', {cwd: config.src})
+  .pipe(through.obj(function(file, enc, next) {
+    // Make index.html appear to be in the same directory as
+    // bower_components for pretty paths.
+    file.base = process.cwd()
+    file.path = path.resolve(file.base, 'index.html')
+    this.push(file)
+    next()
+  }))
+  .pipe(wiredep({
+    exclude: [/html5-boilerplate/],
+  }))
+  .pipe(gulp.dest(config.src))
+})
 
 gulp.task('jshint', function() {
-  return gulp.src(config.scripts)
-    .pipe(changed('.tmp'))
-    .pipe(jshint())
-    .pipe(jshint.reporter(jshintStylish))
-    .pipe(jshint.reporter('fail'))
-    .pipe(size({showFiles: true}))
-    .pipe(gulp.dest('.tmp'))
+  return gulp.src('**/*.js', {cwd: config.src})
+  .pipe(changed(config.dst))
+  .pipe(template(config))
+  .pipe(jshint({predef: ['console']}))
+  .pipe(jshint.reporter(jshintStylish))
 })
 
-gulp.task('html', function() {
-  var cssPipeline = lazypipe()
-    .pipe(autoprefixer, 'last 1 version', {cascade: true})
-    .pipe(csso)
+gulp.task('copy',
+  ['bower'].concat(config.dev ? ['symlink'] : []),
+  function()
+{
+  var jsPipe = [
+    sourcemaps.init(),
+    sourcemaps.write(),
+    'concat',
+    sourcemaps.init({loadMaps: true}),
+    uglify({mangle: false}),
+    rev(),
+    sourcemaps.write('.'),
+  ]
 
-  return gulp.src(config.html)
-    .pipe(changed(config.dst))
-    .pipe(template(config))
-    .pipe(useref.assets())
-    // need this to properly report path misconfiguration
-    .on('error', gutil.log)
-    //.pipe(if_('*.js', uglify()))
-    //.pipe(if_('*.css', cssPipeline()))
-    .pipe(useref.restore())
-    .pipe(useref())
-    .pipe(gulp.dest(config.dst))
-    .pipe(size({showFiles: true}))
-})
-
-gulp.task('copy', function() {
-  return gulp.src(config.files)
-    .pipe(changed(config.dst))
-    .pipe(gulp.dest(config.dst))
-    .pipe(size({showFiles: true}))
-})
-
-gulp.task('build', ['jshint', 'html', 'copy'])
-
-gulp.task('clean', function(cb) {
-  rimraf(config.dst, function() {
-    rimraf('.tmp', cb)
+  var htmlPipe = usemin({
+    css: [
+      template(config),
+      autoprefixer(),
+      csso(),
+      rev()
+    ],
+    rawjs: jsPipe,
+    js: [template(config), ngAnnotate()].concat(jsPipe),
   })
+
+  return gulp.src(config.src + '/**')
+  .pipe(changed(config.dst))
+  .pipe(gulpif(/.*\.(html|css|js)/, template(config)))
+  .pipe(gulpif(!config.dev, gulpif(/.*\/index\.html/, htmlPipe)))
+  .pipe(gulp.dest(config.dst))
+  .pipe(size({title: 'copy', showFiles: true}))
 })
+
+gulp.task('build', ['jshint', 'copy'])
 
 gulp.task('serve', function(cb) {
   var server = connect()
-    .use(connect.static(config.dst))
-    .listen(config.port)
-    .on('error', function(error) {
-      gutil.log(gutil.colors.red('error: ') + 'failed to start server')
-      cb(error)
-    })
-    .on('listening', function() {
-      var host = os.hostname()
-      var port = this.address().port
-      var url  = 'http://' + host + ':' + port
-      gutil.log('serving at ' + gutil.colors.magenta(url))
-      open(url)
-      cb()
-    })
+  .use(connect.livereload())
+  .use(connect.static(config.dst))
+  .listen(config.port)
+  .on('error', function(error) {
+    gutil.log(gutil.colors.red('error: ') + 'failed to start server')
+    cb(error)
+  })
+  .on('listening', function() {
+    var host = os.hostname()
+    var port = this.address().port
+    var url  = 'http://' + host + ':' + port
+    gutil.log('serving at ' + gutil.colors.magenta(url))
+    opn(url)
+    cb()
+  })
 })
 
 gulp.task('watch', ['build', 'serve'], function() {
   livereload.listen()
+  gulp.watch('bower.json', ['wiredep'])
   gulp.watch(config.src + '/**', ['build'])
   gulp.watch(config.dst + '/**').on('change', function(file) {
     livereload.changed(file.path)
   })
+})
+
+gulp.task('clean', function(cb) {
+  del(['dev', 'prod', 'bower_components'], cb)
 })
 
 gulp.task('default', ['build'])
